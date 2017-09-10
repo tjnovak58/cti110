092dc064848073a1b0a11cc12f5d7c578b409ee5
tjnovak58 <31831965+tjnovak58@users.noreply.github.com>
2017-09-10 19:50:32 -0400

    Add files via upload
    First Submission

---
+++ M2HW1_DistanceTraveled_TimothyNovak.py
@@ -0,0 +1,17 @@
+# CTI-110
+# M2HW1 - Distance Traveled
+# Timothy Novak
+# 09/10/17
+#
+# Define the speed the car is traveling.
+speed = 70
+
+# Calculate the distance traveled after 6 hours, 10 hours, and 15 hours. 
+distanceAfter6 = speed * 6
+distanceAfter10 = speed * 10
+distanceAfter15 = speed * 15
+
+# Display the distance traveled after 6 hours, 10 hours, and 15 hours.
+print('The number of miles traveled after 6 hours is ', distanceAfter6)
+print('The number of miles traveled after 10 hours is ', distanceAfter10)
+print('The number of miles traveled after 15 hours is ', distanceAfter15)
